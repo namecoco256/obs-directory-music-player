c1e3782ae9a0d3aa39e58e3770279c4dffd8cf6c
namecoco256 <harinezumi-o_n-o_k-2016@outlook.jp>
2023-05-20 14:43:09 +0900

    gui-template

--- obs-directory-music-player.py
+++ obs-directory-music-player.py
@@ -1,9 +1,24 @@
 import obsws_python as obs
+import PySimpleGUI as sg
+import glob
 
-host = "localhost"
-port = 4455
+sg.theme("DarkBlue")
 
+layout=[
+  [sg.Text("OBS-Directory-Music-Player")],
+  [sg.Text("port"),sg.InputText("4455",key="port")],
+  [sg.Text("password"),sg.InputText("",key="password")],
+  [sg.Button("OK",key="ok")],
+  [sg.Button("test",key="test")]]
+window=sg.Window("OBS-Directory-Music-Player",layout)#Guiの設定
 
 cl = obs.ReqClient(host='localhost', port=4455)
 
-cl.set_input_settings("testmedia",{"local_file": "D:\Music\Gomathrow.ogg"},True)
+while True:
+  event,values=window.read()
+  if event==sg.WIN_CLOSED:
+    break
+  if event=="ok":
+    cl = obs.ReqClient(host='localhost', port=values["port"], password=values["password"])
+  if event=="test":
+    cl.set_input_settings("testtext",{"text": "にゃんこ"},True)
